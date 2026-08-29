e14d2df2ef71d92a39d15b7b9116909605e8da42
Buddhajyoti Roy <buddhajyotiroy2005@gmail.com>
2026-08-30 01:43:11 +0530

    Revised Main.dart

--- zero_remote_app/test/widget_test.dart
+++ zero_remote_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:zero_remote_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ZeroRemoteApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
